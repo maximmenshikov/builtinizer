54e3292a80a83f6904c382220c67fcb9afe013ef
Maxim Menshikov <maxim@menshikov.org>
2020-12-29 21:46:21 +0300

    [#9] generate_constexpr: fix ptrdiff_t replacement, create files rather than write to stdout

--- scripts/generate_constexpr.sh
+++ scripts/generate_constexpr.sh
@@ -6,25 +6,37 @@ set -e
 set -o pipefail
 
 url=https://raw.githubusercontent.com/llvm/llvm-project/main/clang/include/clang/Basic/Builtins.def
+params="--constexpr --constexpr-math --ignore-half
+        --replace ptrdiff_t:__PTRDIFF_TYPE__
+        --ignore __builtin___CFStringMakeConstantString
+        --ignore __builtin___NSStringMakeConstantString
+        --ignore __builtin_convertvector
+        --ignore __builtin_addressof"
+params_list="--list"
+out_file="constexpr-builtins.hookdef"
+out_file_list="constexpr-builtins.gcc.compdef"
 
 top_folder="$(cd "$(dirname "$(which "$0")")"/.. ; pwd -P)"
 . "${top_folder}/scripts/lib.sh"
 
 pushd "${top_folder}/src" > /dev/null
-	# Download Builtins.def
-	wget -q "${url}" -O Builtins.def
+    # Download Builtins.def
+    wget -q "${url}" -O Builtins.def
 popd > /dev/null
 
 pushd "${top_folder}" > /dev/null
-	out=$(./scripts/build.sh)
-	if [ "$?" != "0" ] ; then
-		fail "${out}"
-	fi
-	./build/builtinizer --constexpr \
-						--constexpr-math \
-						--ignore-half \
-						--replace ptrdiff_t:__PTRDIFF_T__ \
-						--ignore __builtin___CFStringMakeConstantString \
-						--ignore __builtin___NSStringMakeConstantString \
-						--ignore __builtin_convertvector
+    out=$(./scripts/build.sh)
+    if [ "$?" != "0" ] ; then
+        fail "${out}"
+    fi
+
+    # Signatures of constexpr builtins
+    echo "#if defined(__GNUG__) && !defined(__clang__)" > "${out_file}"
+    ./build/builtinizer $params >> "${out_file}"
+    echo "#endif" >> "${out_file}"
+
+    # List of constexpr builtins
+    echo "undefine-builtin: $(./build/builtinizer $params $params_list)" \
+        >> "${out_file_list}"
+
 popd > /dev/null
